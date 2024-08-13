c519e5e352ce836ac9f9dc22ceb02f2dd54a7bf4
KemasRaihan <b6024493@my.shu.ac.uk>
2024-08-13 22:38:50 +0100

    Added classes for Player and Game.

---
+++ src/Game.java
@@ -0,0 +1,7 @@
+public interface Game {
+    public void playRound();
+    public String playRock(String choice);
+    public String playPaper(String choice);
+    public String playScissors(String choice);
+
+}
